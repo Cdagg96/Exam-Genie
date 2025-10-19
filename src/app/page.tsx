dfd98549d63520a6c8922ce9c044d529b8777de6
Garrett <gf971522@ohio.edu>
2025-10-19 18:10:21 -0400

    Added info pop up boxes on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,87 @@
+"use client";
+
 import Image from "next/image";
 import NavBar from "@/components/navbar";
+import { useState } from "react";
 
 export default function Home() {
+  // Using react state to manage info display on hover
+  const [displayCreateTestInfo, setDisplayCreateTestInfo] = useState(false);
+  const [displayViewQuestionsInfo, setDisplayViewQuestionsInfo] = useState(false);
   return (
+    // Centers content and applies background gradient
     <div className="items-center justify-items-center min-h-screen p-8 bg-gradient-to-b from-[#EFF6FF] to-white">
       <NavBar />
-      <h1 className="text-7xl text-center pt-40 font-bold bg-gradient-to-r from-blue-400 via-cyan-400 to-blue-600 bg-clip-text text-transparent">Exam Genie</h1>
 
-      <div className="space-y-10 pt-20">
-        <button className="block w-110 h-15 bg-stone-800 text-white text-xl rounded-2xl shadow hover:bg-black">Create Test</button>
-        <button className="block w-110 h-15 bg-stone-800 text-white text-xl rounded-2xl shadow hover:bg-black">View Your Questions</button>
+      {/* Home page title */}
+      <h1 className="text-7xl text-center pt-40 font-bold bg-gradient-to-r from-blue-400 via-cyan-400 to-blue-600 bg-clip-text text-transparent">
+        Exam Genie
+      </h1>
+
+      {/* Position buttons/info icons */}
+      <div className="space-y-10 mt-20 w-fit">
+        {/* Info icon positioned with Create Test button */}
+        <div className="relative">
+          <div
+            onMouseEnter={() => setDisplayCreateTestInfo(true)}
+            onMouseLeave={() => setDisplayCreateTestInfo(false)}
+          >
+            {/* Info icon from Heroicons */}
+            <svg 
+              xmlns="http://www.w3.org/2000/svg" 
+              fill="none" viewBox="0 0 24 24" 
+              strokeWidth={1.5} stroke="currentColor" 
+              className="absolute size-6 -top-6 -right-2"
+            >
+              <path 
+                strokeLinecap="round" 
+                strokeLinejoin="round" 
+                d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" 
+              />
+            </svg>
+            {/* Info display for Create Test button */}
+            {displayCreateTestInfo && (
+              <div className="absolute -top-24 -right-70 w-64 p-4 bg-gradient-to-b from-[#EFF6FF] to-white border border-stone-800 rounded-lg text-sm text-stone-800">
+                Click to create a new test by selecting saved questions.
+              </div>
+            )}
+          </div>
+          {/* Button style */}
+          <button className="block w-110 h-15 bg-stone-800 text-white text-xl rounded-2xl shadow hover:bg-black">
+            Create Test
+          </button>
+        </div>
+
+        <div className="relative">
+          {/* Info icon positioned with View Your Questions button */}
+          <div
+            onMouseEnter={() => setDisplayViewQuestionsInfo(true)}
+            onMouseLeave={() => setDisplayViewQuestionsInfo(false)}
+          >
+            <svg 
+              xmlns="http://www.w3.org/2000/svg" 
+              fill="none" viewBox="0 0 24 24" 
+              strokeWidth={1.5} stroke="currentColor" 
+              className="absolute size-6 -top-6 -right-2"
+            >
+              <path
+                strokeLinecap="round" 
+                strokeLinejoin="round" 
+                d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" 
+              />
+            </svg>
+            {/* Info display for View Your Questions button */}
+            {displayViewQuestionsInfo && (
+              <div className="absolute -top-24 -right-70 w-64 p-4 bg-gradient-to-b from-[#EFF6FF] to-white border border-stone-800 rounded-lg text-sm text-stone-800">
+                Click to view and manage saved questions.
+              </div>
+            )}
+          </div>
+
+          <button className="block w-110 h-15 bg-stone-800 text-white text-xl rounded-2xl shadow hover:bg-black">
+            View Your Questions
+          </button>
+        </div>
       </div>
     </div>
   );
