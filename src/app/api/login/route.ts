c85fbe2465d4d5d6afdae1e4decf21bd66ad97d3
Garrett <gf971522@ohio.edu>
2025-10-22 13:47:15 -0400

    Added new api route for login to check if email and password exist in database

---
+++ src/app/api/login/route.ts
@@ -0,0 +1,47 @@
+import { NextResponse, NextRequest } from "next/server";
+import clientPromise from "@/libs/mongo";
+
+export async function POST(req:Request) {
+    try {
+        const {email, password} = await req.json();
+
+        // validate the data coming in
+        if (!email || !password) {
+            return NextResponse.json({ message: "Missing fields" }, { status: 400 });
+        }
+        
+        // check if the user exists in the database with the given email
+        const client = await clientPromise;
+        const db = client.db(process.env.MONGODB_DB);
+        const user = await db.collection("users").findOne({ email });
+
+        // if the user does not exist, return email not found
+        if(!user) {
+            return NextResponse.json(
+                {ok: false, message: "Email not found"},
+                {status: 404}
+            );
+        }
+
+        // if the password does not match, return incorrect password
+        if(user.password !== password) {
+            return NextResponse.json(
+                {ok: false, message: "Incorrect password"},
+                {status: 401}
+            );
+        }
+
+        // if both email and password match, return success
+        return NextResponse.json(
+            {ok: true, message: "Login successful", userId: user._id},
+            {status: 200}
+        );
+    } catch (error) {
+        // throw an error message for any unexpected server error
+        console.error("Error during login: ", error)
+        return NextResponse.json(
+            {ok: false, error: "Internal server error"},
+            {status: 500}
+        );
+    }       
+}
