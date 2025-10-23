dcfd361959be16c7060e20c7700d4a12bc2e88ea
Aidan-Rogers <ar320621@ohio.edu>
2025-10-23 12:44:39 -0400

    adds loaded to usedstate/local storage to make sure that user stays loged in when switching page

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import LoginModal from "./LoginModal";
@@ -8,6 +8,24 @@ import LoginModal from "./LoginModal";
 export default function Navbar() {
   const [isLoginOpen, setIsLoginOpen] = useState(false);
   const [LoggedIn, setLoggedIn] = useState(false);
+  const [loaded, setLoaded] = useState(false);
+
+
+  // load log in stat from local storage
+  useEffect(() => {
+    const storedLoginState = localStorage.getItem("LoggedIn");
+    if (storedLoginState === "true") {
+      setLoggedIn(true);
+    }
+    setLoaded(true); //only render buttons after this
+  }, []);
+
+  // Save login state to local storage when it changes
+  useEffect(() => {
+    if (loaded) {
+      localStorage.setItem("LoggedIn", LoggedIn.toString());
+    }
+  }, [LoggedIn, loaded]);
 
   return (
     <>
@@ -26,29 +44,29 @@ export default function Navbar() {
           <Link href="../exam_gen/" className="text-stone-800 hover:text-black">Generator</Link>
           <Link href="#" className="text-stone-800 hover:text-black">Help</Link>
           <Link href="../contact/" className="text-stone-800 hover:text-black">Contact</Link>
-          
-          {!LoggedIn &&
+
+          {loaded && !LoggedIn &&
             <button
-            onClick={() => setIsLoginOpen(true)}
-            className="w-20 h-8 bg-stone-800 text-white text-sm rounded-2xl shadow hover:bg-black flex items-center justify-center -mt-1">
-            Sign in
+              onClick={() => setIsLoginOpen(true)}
+              className="w-20 h-8 bg-stone-800 text-white text-sm rounded-2xl shadow hover:bg-black flex items-center justify-center -mt-1">
+              Sign in
             </button>
           }
           {/*Signout Button*/}
-          {LoggedIn &&
+          {loaded && LoggedIn &&
             <button
-            onClick={() => setLoggedIn(false)}
-            className="w-20 h-8 bg-stone-800 text-white text-sm rounded-2xl shadow hover:bg-black flex items-center justify-center -mt-1">
-            Sign Out
+              onClick={() => setLoggedIn(false)}
+              className="w-20 h-8 bg-stone-800 text-white text-sm rounded-2xl shadow hover:bg-black flex items-center justify-center -mt-1">
+              Sign Out
             </button>
           }
 
-          
+
         </div>
       </div>
 
-      
-      <LoginModal isOpen={isLoginOpen} onClose={() => setIsLoginOpen(false)} LoggedIn ={LoggedIn} setLoggedIn={setLoggedIn} />
+
+      <LoginModal isOpen={isLoginOpen} onClose={() => setIsLoginOpen(false)} LoggedIn={LoggedIn} setLoggedIn={setLoggedIn} />
     </>
   );
 }
