91f3d5735226a08f42bae3009f9bbb7ffe4014f6
Cdagg96 <cdagg96@gmail.com>
2025-11-07 17:39:32 -0500

    Super small bug fix with edit link

--- src/app/past_exams/page.tsx
+++ src/app/past_exams/page.tsx
@@ -293,9 +293,9 @@ export default function PastExams() {
                                                 </td>
                                                 <td className="px-6 py-4 whitespace-nowrap text-sm font-medium">
                                                     <div className="flex items-center justify-center gap-4">
-                                                        <button className="text-blue-600 hover:text-blue-900">
+                                                        <Link href={`/edit_exam/${exam._id}`} className="text-blue-600 hover:text-blue-900">
                                                             Edit
-                                                        </button>
+                                                        </Link>
                                                         <button className="text-red-600 hover:text-red-900" onClick={() => handleDeleteClick(exam._id, exam.title)}>
                                                             Delete
                                                         </button>
